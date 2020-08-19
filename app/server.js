b69634ae322b7b0379ca6edde67866cd4e50a67c
ricky3knowhere <ricky03senju@gmail.com>
2020-08-19 17:29:28 +0700

    create MVC model

--- app/server.js
+++ app/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 4000
 
 app.set('view engine', 'twig')
+app.use(bodyParser.urlencoded({extended : true}))
 
 const user = require('../routes/user')
 
@@ -10,9 +12,14 @@ app.get('/',(req,res) => {
     res.send('server')
 })
 
-app.get('/user_by_name/:name',user.by_name)
-app.get('/user_by_id/:id',user.by_id)
-
+// app.get('/user_by_name/:name', user.by_name)
+app.get('/user/new', user.form_create)
+app.post('/user/new', user.post_data)
+app.get('/user/:id/delete', user.delete_id)
+app.get('/user/:id/update', user.form_update)
+app.post('/user/:id', user.update_by_id)
+app.get('/user/:id', user.by_id)
+app.get('/user', user.all)
 
 app.listen(port,() => {
     console.log(`Example app listening at https://localhost:${port}`)
--- routes/user.js
+++ routes/user.js
@@ -1,29 +1,120 @@
-const {Sequelize} = require('sequelize')
+const {Sequelize, DataTypes, Op} = require('sequelize')
 
 const sequelize =new Sequelize({
   dialect : 'mysql',
   host : 'localhost',
   port : '3306',
   username : 'employee_data',
-  password : 'employee_data'
+  password : 'employee_data',
+  database : 'employee_data'
 })
 
-const by_name = (req,res) => {
-  res.render(
-    'user_name',
-    {
-      name: req.params.name
-    })
-}
+const User = sequelize.define('User',
+  {
+    id : {
+      type : DataTypes.INTEGER,
+      autoIncrement : true,
+      primaryKey : true
+    },
+    name : {
+      type : DataTypes.STRING,
+      allowNull : false
+    },
+  },{
+    tableName : 'user',
+    timestamps : false
+  }
+)
 
-const by_id = (req,res) => {
-  sequelize.authenticate().then(
-    () => res.send('Successfull.'),
-    () => res.send('Unsuccessfull.')
+const by_name = (req,res) => {
+  User.findOne({
+    where : {
+      name : {
+        [Op.eq] : req.params.name
+      }
+    }
+  }).then(
+    (data) => {
+      res.send(data)
+    },
+    (error) => res.send(error)
   ) 
 }
 
+const by_id = (req,res) => {
+  User.findByPk(req.params.id).then(
+    (user) => {
+      res.render('single_user', { user })
+    },
+    (error) => res.send(error)
+  ) 
+}
+
+const form_create = (req,res) => {
+  res.render('form_create')
+}
+
+const all = (req,res) => {
+  User.findAll().then(
+    (users) => res.render('user_all',{users}),
+    (err) => res.send(err)
+  )
+}
+
+const post_data = (req,res) => {
+  // res.send(req.body)
+  User.create(req.body)
+  .then(
+    (user) => res.render('single_user', {user}),
+    (err) => res.send(err)
+  )
+}
+
+const delete_id = (req,res) => {
+  User.destroy({ where : {
+    id : {
+      [Op.eq] : req.params.id
+    }
+  }
+  })
+  .then(
+    () => res.redirect('/user'),
+    (err) => res.send(err)
+  )
+}  
+
+const form_update = (req,res) => {
+  User.findByPk(req.params.id)
+  .then(
+    (user) => {
+      res.render('form_update',{ user })
+  },
+  (err) => res.send(err)
+  )
+}
+
+const update_by_id = (req,res) => {
+  User.update(
+    req.body,
+    { 
+      where : {
+        id : req.params.id
+      }
+    }
+  )
+  .then(
+    (user) => res.redirect(`/user/${req.params.id}`),
+    (err) => res.send(err)
+  )
+}  
+
 module.exports = {
   by_name,
-  by_id
+  by_id,
+  form_create,
+  post_data,
+  all,
+  delete_id,
+  form_update,
+  update_by_id
 }
