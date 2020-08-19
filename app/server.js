b969577c1612bc9807762116b0c6f6369e106e54
ricky3knowhere <ricky03senju@gmail.com>
2020-08-19 11:17:14 +0700

    create app directory

--- app/server.js
+++ app/server.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+app.set('view engine', 'twig')
+
+const user = require('../routes/user')
+
 app.get('/',(req,res) => {
     res.send('server')
 })
+
+app.get('/user/:name',user.by_name)
 
 app.listen(port,() => {
     console.log(`Example app listening at https://localhost:${port}`)
